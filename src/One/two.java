5e2375a07392f61f8e3d9ea27010e0021f5fa60c
ilaiyaraja1 <ilaiyaraja.bca@gmail.com>
2021-08-24 09:23:11 +0530

    modifiers

---
+++ src/One/two.java
@@ -0,0 +1,25 @@
+package One;
+
+public class two {
+	public static void main(String[] args) {
+        
+	/*
+	 * modifiers
+	 * 1.public
+	 * 2.private
+	 * 3.protected
+	 * 
+	 * non- Accessmodifiers
+	 * 
+	 * 1.static
+	 * 2.final
+	 * 3.abstract
+	 * 4.synchronized
+	 * 5.volatile
+	 * 6.transient
+	 * ------------------------------------
+	 * 
+	 */
+		
+	}
+}
